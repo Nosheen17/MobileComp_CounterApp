e7ad38238d73a65915fd8a44e1500e28d66f71db
Nosheen17 <hayat.mehreen17@gmail.com>
2022-04-30 16:39:56 +0500

    Declaring Variables

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
